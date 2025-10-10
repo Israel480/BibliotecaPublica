7babf37527486908abcf07a7829dfad4c5d8d1db
Israel480 <israelwadmi5@gmail.com>
2025-10-10 11:36:16 -0300

    Leitor(class)

---
+++ publica/src/main/java/br/edu/org/biblioteca/publica/domain/model/Leitor.java
@@ -0,0 +1,31 @@
+package br.edu.org.biblioteca.publica.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Leitores")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Leitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+    private String endereco;
+    private long telefone;
+
+
+    @Column(unique = true)
+    private String login;
+    @ToString.Exclude
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 120)
+    private String email;
+
+}
