c6e0850d9a74075218465dae3e997ee3645f6da5
wans-ranier <wansranier@gmail.com>
2025-10-13 16:24:10 -0300

    Criei as classes referentes ao ALuguel e seu relacionamento com Leitor e Livros(e suas variações em DTO), também os DTO's de Aluguel:AluguelResponseDTO(saida) e ALuguelResquestDTO(entrada do cliente). fiz tabem a mescleegemde duas branchs, pois a main não exitia para mim e não estava commo padrão

--- src/main/java/br/edu/org/biblioteca/publica/domain/dto/LeitorLowDTO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.org.biblioteca.publica.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class LeitorLowDTO {
-    private Long id;
-    private String name;
-}
--- src/main/java/br/edu/org/biblioteca/publica/domain/dto/LivroLowDTO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.org.biblioteca.publica.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class LivroLowDTO {
-    private Long id;
-    private String titulo;
-}
